faa8449f891e4ef14c9c36b11bb91b6b72d588dc
Javier Dinamarca <javierdinamarcaxd@gmail.com>
2023-06-09 18:18:04 -0400

    inicio de sesion funcional

--- moduloApp/views.py
+++ moduloApp/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth import login, logout, authenticate
 from django.http import HttpResponse
 from .forms import *
 from .models import *
+from django.contrib import messages
 
 # Create your views here.
 
@@ -54,21 +55,21 @@ def cerrarSesion(request):
 def inicioSesion(request):
     if request.method == 'GET':
         return render(request, 'sistema/inicioSesion.html', {
-        'form': AuthenticationForm
+            'form': AuthenticationForm()
         })
     else:
-
-        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
-        if user is None:
-            return render(request, 'sistema/inicioSesion.html',
-            {
-                'form': AuthenticationForm,
-                'error': "El usuario o contraseña son incorrectos"
-            })
-        else:
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
             login(request, user)
+            messages.success(request, '¡Autenticación exitosa!')
             return redirect('index')
-
+        else:
+            messages.error(request, 'El usuario o contraseña son incorrectos')
+            return render(request, 'sistema/inicioSesion.html', {
+                'form': AuthenticationForm()
+            })
 
 def sucursales(request):
    # return render(request, 'sucursal/sucursales.html')
