3bef9588249da798d89f9226e376c2fb320df6eb
Javier Dinamarca <javierdinamarcaxd@gmail.com>
2023-06-20 17:18:11 -0400

    Añadir cambios y archivos nuevos

---
+++ static/js/botones.js
@@ -0,0 +1,15 @@
+$ ("#confirmacion").click(function(){
+  Swal.fire({
+    type: 'success',
+    tittle: 'mensaje',
+    text: 'texto de prueba',
+  });
+});
+
+$ ("#confirmacion").click(function(){
+  Swal.fire({
+    type: 'warning',
+    tittle: 'mensaje',
+    text: 'texto de prueba',
+  });
+});
